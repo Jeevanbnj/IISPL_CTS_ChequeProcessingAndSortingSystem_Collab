576beb8429eef5e4333fe37d654fd907ff170ee1
iispl <iispl@iispl>
2026-07-31 17:25:38 +0530

    Added all code to ChequeValidator class

--- src/com/iispl/service/ChequeValidator.java
+++ src/com/iispl/service/ChequeValidator.java
@@ -1,10 +1,17 @@
 package com.iispl.service;
 
 
+import java.util.ArrayList;
 import java.util.List;
 import com.iispl.enums.ChequeStatus;
 import com.iispl.model.Cheque;
+import com.iispl.vallidation.AccountNumberValidationRule;
+import com.iispl.vallidation.ChequeAmountValidationRule;
+import com.iispl.vallidation.ChequeNumberValidationRule;
 import com.iispl.vallidation.ChequeValidationRule;
+import com.iispl.vallidation.DateValidationRule;
+import com.iispl.vallidation.DrawerNameValidationRule;
+import com.iispl.vallidation.PresentingBankValidationRule;
 
 
 public class ChequeValidator {
@@ -12,16 +19,36 @@ public class ChequeValidator {
 	List<ChequeValidationRule> rules ;
 	
 	public ChequeValidator() {
-		//Initialize the rules list and add all the rules
+
+		rules = new ArrayList<ChequeValidationRule>();
+		
+		rules.add(new ChequeNumberValidationRule());
+		rules.add(new AccountNumberValidationRule());
+		rules.add(new DrawerNameValidationRule());
+		rules.add(new PresentingBankValidationRule());
+		rules.add(new ChequeAmountValidationRule());
+		rules.add(new DateValidationRule());
+
 	}
 	
 	public void addRule(ChequeValidationRule rule) {
 		
+		rules.add(rule);
 		
 	}
 	
 	public ChequeStatus validate(Cheque cheque) {
-		return null;
+		
+		for (ChequeValidationRule rule : rules) {
+			ChequeStatus status = rule.validate(cheque);
+			
+			if (status.equals(ChequeStatus.REJECTED)) {
+				return status;
+			}
+		}
+		
+		System.out.println("All validations are passed");
+		return ChequeStatus.ACCEPTED;
 	}
 
 }
