f699323fb4339c215af88212af27e3ac80dbea3a
iispl <iispl@iispl>
2026-07-31 14:26:22 +0530

    Updated ChequeNumber and DrawerName Validation

--- src/com/iispl/vallidation/ChequeNumberValidationRule.java
+++ src/com/iispl/vallidation/ChequeNumberValidationRule.java
@@ -8,7 +8,17 @@ public class ChequeNumberValidationRule implements ChequeValidationRule {
 	@Override
 	public ChequeStatus validate(Cheque cheque) {
 		// TODO Auto-generated method stub
-		return null;
+		String ChequeNumber=cheque.getChequeNumber();
+		
+		if(ChequeNumber==null || ChequeNumber.isBlank()) {
+			System.out.println("ChequeNumber Should not be Null or Empty");
+			return ChequeStatus.REJECTED;
+		}
+		if(ChequeNumber.length() <= 0 ) {
+			System.out.println("Cheque Number length Should be greater then zero");
+			return ChequeStatus.REJECTED;	
+		}
+		return ChequeStatus.PENDING;
 	}
 
 }
